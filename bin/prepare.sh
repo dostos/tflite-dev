efce793ef83ceef769d24009e28e9b3d26c3d99b
Jingyu Lee <dostos10@gmail.com>
2021-12-16 17:47:32 +0900

    propagate all devices

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -1,6 +1,5 @@
-adb -d push benchmark_model //data\\local\\tmp
-adb -d push plan.json //data\\local\\tmp
-adb -d push ../gdbserver //data\\local\\tmp
-
-adb -d shell chmod 777 //data\\local\\tmp\\benchmark_model
-adb -d shell chmod 777 //data\\local\\tmp\\gdbserver
+sh adb-all.sh push benchmark_model //data\\local\\tmp
+sh adb-all.sh push plan.json //data\\local\\tmp
+sh adb-all.sh push ../gdbserver //data\\local\\tmp
+sh adb-all.sh shell chmod 777 //data\\local\\tmp\\benchmark_model
+sh adb-all.sh shell chmod 777 //data\\local\\tmp\\gdbserver
--- bin/run_benchmark.sh
+++ bin/run_benchmark.sh
@@ -1,3 +1,3 @@
-adb -d shell rm //data\\local\\tmp\\model_profile_subgraph.json
-adb -d shell su -c //data\\local\\tmp\\benchmark_model --json_path=//data\\local\\tmp\\plan.json ${@:1}
-adb -d pull //data\\local\\tmp\\model_execution_log.csv
+adb shell rm //data\\local\\tmp\\model_profile_subgraph.json
+adb shell //data\\local\\tmp\\benchmark_model --json_path=//data\\local\\tmp\\plan.json ${@:1}
+adb pull //data\\local\\tmp\\log.csv
